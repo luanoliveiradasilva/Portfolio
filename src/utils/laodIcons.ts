340531a8b32eef3348dad92065672de7708faf1a
= <=>
2024-08-01 15:41:25 +0100

    update

--- src/utils/laodIcons.ts
+++ src/utils/laodIcons.ts
@@ -1,12 +1,11 @@
 import iconTech from "/assets/icons/techs/devicon_csharp.svg";
 import iconTech1 from "/assets/icons/techs/devicon_java.svg";
 import iconTech2 from "/assets/icons/techs/devicon_kotlin.svg";
-import iconTech3 from "/assets/icons/techs/devicon_csharp.svg";
-import iconTech4 from "/assets/icons/techs/devicon_spring.svg";
-import iconTech5 from "/assets/icons/techs/skill-icons_javascript.svg";
-import iconTech6 from "/assets/icons/techs/skill-icons_react-dark.svg";
-import iconTech7 from "/assets/icons/techs/skill-icons_tailwindcss-dark.svg";
-import iconTech8 from "/assets/icons/techs/devicon_unity.svg";
+import iconTech3 from "/assets/icons/techs/devicon_spring.svg";
+import iconTech4 from "/assets/icons/techs/skill-icons_javascript.svg";
+import iconTech5 from "/assets/icons/techs/skill-icons_react-dark.svg";
+import iconTech6 from "/assets/icons/techs/skill-icons_tailwindcss-dark.svg";
+import iconTech7 from "/assets/icons/techs/devicon_unity.svg";
 
 import iconProject from "/assets/img/projects/TC 1.svg";
 import iconProject1 from "/assets/img/projects/transferir 1.svg";
@@ -20,8 +19,7 @@ const iconsTech = [
     iconTech4,
     iconTech5,
     iconTech6,
-    iconTech7,
-    iconTech8
+    iconTech7
 ];
 
 const iconsProjects = [

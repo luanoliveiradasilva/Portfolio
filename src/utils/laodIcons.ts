121686e17597c678ea68dccb12284f29f194daaf
= <=>
2024-08-01 14:55:00 +0100

    update

---
+++ src/utils/laodIcons.ts
@@ -0,0 +1,23 @@
+import icon from "/assets/icons/techs/devicon_csharp.svg";
+import icon1 from "/assets/icons/techs/devicon_java.svg";
+import icon2 from "/assets/icons/techs/devicon_kotlin.svg";
+import icon3 from "/assets/icons/techs/devicon_csharp.svg";
+import icon4 from "/assets/icons/techs/devicon_spring.svg";
+import icon5 from "/assets/icons/techs/skill-icons_javascript.svg";
+import icon6 from "/assets/icons/techs/skill-icons_react-dark.svg";
+import icon7 from "/assets/icons/techs/skill-icons_tailwindcss-dark.svg";
+import icon8 from "/assets/icons/techs/devicon_unity.svg";
+
+const icons = [
+    icon,
+    icon1,
+    icon2,
+    icon3,
+    icon4,
+    icon5,
+    icon6,
+    icon7,
+    icon8
+];
+
+export default icons;

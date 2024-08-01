fd25f211f88fa13eae1334ef83f5d19604a8e394
= <=>
2024-08-01 09:54:10 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,58 +5,42 @@ import About from './pages/About';
 import Skill from './pages/Skill';
 import Project from './pages/Project';
 import Contact from './pages/Contact';
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Footer from './components/Footer';
 import { Element } from 'react-scroll';
 
-function RootLayout() {
-  return (
-    <>
-      <Header />
-      <div className='w-auto h-full px-36 pt-7'>
-        <Outlet />
-      </div>
-      <Footer />
-    </>
-  );
-}
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      {
-        path: '/',
-        element: (
-          <>
-            <Element name="Home">
-              <Home />
-            </Element>
-            <Element name="About">
-              <About />
-            </Element>
-            <Element name="Skills">
-              <Skill />
-            </Element>
-            <Element name="Projects">
-              <Project />
-            </Element>
-            <Element name="Contactme">
-              <Contact />
-            </Element>
-          </>
-        )
-      }
-    ]
-  }
-]);
-
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Header />
+    }
+  ])
   return (
-    <RouterProvider router={router} />
-  );
+    <>
+      <div className='w-auto h-full px-36 pt-7'>
+        <RouterProvider router={router} />
+
+        <Element name="Home">
+          <Home />
+        </Element>
+        <Element name="About">
+          <About />
+        </Element>
+        <Element name="Skills">
+          <Skill />
+        </Element>
+        <Element name="Projects">
+          <Project />
+        </Element>
+        <Element name="Contactme">
+          <Contact />
+        </Element>
+        <Footer />
+      </div>
+    </>
+  )
 }
 
 export default App
